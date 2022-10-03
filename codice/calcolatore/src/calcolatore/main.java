704cb8d80c8d6f3fe0112b37db50217099e49186
LLongaretti <114903758+LLongaretti@users.noreply.github.com>
2022-10-03 10:21:50 +0200

    add

--- codice/calcolatore/src/calcolatore/main.java
+++ codice/calcolatore/src/calcolatore/main.java
@@ -1,10 +1,17 @@
 package calcolatore;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.print("prova");
+		Scanner reader = new Scanner(System.in);
+			System.out.println("primo numero");
+			int x = reader.nextInt();
+			System.out.println("secondo numero");
+			int y = reader.nextInt();
+			System.out.println(x+y);
 	}
 
 }
